9ff20db33ea7a938beefe70db72a334d18217f34
Restyled.io <commits@restyled.io>
2020-10-01 16:45:44 +0000

    Restyled by autopep8

--- sum.py
+++ sum.py
@@ -5,11 +5,10 @@ b = 2
 
 # Add two numbers
 sum = float(a) + float(b)
-#you can also write it as:
-sum=float(a+b)
+# you can also write it as:
+sum = float(a+b)
 
 # Display the sum
 print('The sum of {0} and {1} is {2}'.format(a, b, sum))
-#in python 3.6 or above you can write it as
+# in python 3.6 or above you can write it as
 print(f"sum of {a} and {b} is {sum}")
-
